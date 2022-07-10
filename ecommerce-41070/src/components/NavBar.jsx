83c9abadc5ec59e48206555ae6003e1f82d8bfc7
Pilar Igartua <piligartua@gmail.com>
2022-07-10 18:41:06 -0300

    ecommerce

---
+++ ecommerce-41070/src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+const Menu = () => {
+    return (
+        <div style={{backgroundColor: "Black", color: "white", fontSize: "20px"}}>
+
+            <span> Burguer Market Company</span> 
+
+            <nav style={{backgroundColor: "Black", color: "white",}}>
+               <ul style={{display: "inline"}}>
+                <li>
+                    <a href="" style={{textDecoration: "none", color: "white"}}>Inicio</a>
+                </li>
+                <li>
+                    <a href="" style={{textDecoration: "none", color: "white"}}>Quienes somos</a>
+                </li>
+                <li>
+                    <a href="" style={{textDecoration: "none", color: "white"}}>Menu</a>
+                </li>
+               </ul>
+            </nav>
+
+        </div>
+        
+    )
+}
+
+export default Menu
