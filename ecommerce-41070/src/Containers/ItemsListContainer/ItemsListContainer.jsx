82128078a86a4aa9d5b37199c1ad1badf26a80a5
Pilar Igartua <piligartua@gmail.com>
2022-07-14 13:04:56 -0300

    contador

--- ecommerce-41070/src/Containers/ItemsListContainer/ItemsListContainer.jsx
+++ ecommerce-41070/src/Containers/ItemsListContainer/ItemsListContainer.jsx
@@ -1,7 +1,28 @@
-const ItemsListContainer = ({greeting}) => {
-    return (
-        <div>{greeting}</div>
-    )
-}
+import {useState} from "react"
 
-export default ItemsListContainer
+const ItemListContainer = ({ greeting }) => {
+   
+    const [ contador , actualizarContador ] = useState(1)
+    
+    const disminuir = () => {    
+        actualizarContador(contador - 1) 
+    }  
+
+    const aumentar = () => {    
+      actualizarContador(contador + 1) 
+    }  
+    
+    console.log('item List Container rendered')
+    
+    return (
+      <div>
+        { greeting }<br></br>
+        { contador } <br />
+        <button onClick = {disminuir}>-</button>
+        <button onClick={aumentar}>+</button>
+      </div>
+    )
+  }
+  
+  export default ItemListContainer
+  //<button onClick = {disminuir}>-</button>
